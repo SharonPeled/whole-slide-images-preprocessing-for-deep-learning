b5305b068f055fc1ed2392e208661407ed5af0fb
SharonPeled <ssharon95@gmail.com>
2024-02-24 14:04:39 +0200

    cosmetic fix

--- src/components/objects/Slide.py
+++ src/components/objects/Slide.py
@@ -38,9 +38,6 @@ class Slide(Image):
         self.set('summary_df_path', os.path.join(os.path.dirname(self.path), summary_df_filename))
 
         self.set('metadata_path', os.path.join(os.path.dirname(self.path), metadata_filename))
-        # self.set('metadata_path', os.path.join(f'/mnt/data/users/sharonpe/slides',
-        #                                        os.path.basename(os.path.dirname(self.path)), metadata_filename))
-        # warnings.warn('-'*10 + 'LOADING METADATA FROM DIFFERENT SLIDE DIR!! ' + '-'*10)
 
         if load_metadata:
             self.load_slide_metadata()
