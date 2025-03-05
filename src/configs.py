eb09600c0d072c8928d7a1be59649ead6e7027f9
sharon <ssharon95@gmail.com>
2025-03-05 11:15:22 +0200

    download adjusments

--- src/configs.py
+++ src/configs.py
@@ -21,7 +21,7 @@ class ConfigsSingletonClass:
         self.config_dict['METADATA_JSON_FILENAME'] = f'metadata_{preprocess_run_name}.json'
         self.config_dict['SUMMARY_DF_FILENAME'] = f'summary_df_{preprocess_run_name}.csv'
         self.config_dict['THUMBNAIL_FILENAME'] = f'thumbnail_{preprocess_run_name}.png'
-        if self.config_dict['PEN_FILTER']['color_palette'] is None:
+        if self.config_dict['PEN_FILTER'] is not None and self.config_dict['PEN_FILTER']['color_palette'] is None:
             self.config_dict['PEN_FILTER']['color_palette'] = get_pen_color_palette()
 
     def paste_current_time(self):
