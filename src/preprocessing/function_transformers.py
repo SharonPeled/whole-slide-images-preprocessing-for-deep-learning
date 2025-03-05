d3e8889b8e9cdbf5d8ed06456ca0a80f37fa33c6
sharon <ssharon95@gmail.com>
2025-03-05 11:27:31 +0200

    download adjusments

--- src/preprocessing/function_transformers.py
+++ src/preprocessing/function_transformers.py
@@ -101,7 +101,7 @@ def filter_pen_reduced_image(slide, color_palette, **kwargs):
 
 def filter_non_tissue_tiles(slide, non_tissue_threshold, otsu_filter, black_filter, pen_filter):
     tile_background_fracs = filter_otsu_reduced_image(slide, **otsu_filter)
-    tile_black_fracs = filter_black_reduced_image(slide, **black_filter) if black_filter is not None else np.zeros_li(tile_background_fracs.shape)
+    tile_black_fracs = filter_black_reduced_image(slide, **black_filter) if black_filter is not None else np.zeros(tile_background_fracs.shape)
     tile_pen_fracs = filter_pen_reduced_image(slide, **pen_filter) if pen_filter is not None else np.zeros(tile_background_fracs.shape)
     filters_array_list = [tile_background_fracs, tile_black_fracs, tile_pen_fracs]
     num_filtered_per_filter = list(map(lambda f: f.sum(), filters_array_list))
