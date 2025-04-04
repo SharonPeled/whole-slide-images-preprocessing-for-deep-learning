ec7eaecd6ec04fd3ca63682b797d161b7a7cb40f
sharon <ssharon95@gmail.com>
2025-04-04 10:03:07 +0300

    test print

--- src/preprocessing/function_transformers.py
+++ src/preprocessing/function_transformers.py
@@ -107,7 +107,7 @@ def filter_non_tissue_tiles(slide, non_tissue_threshold, otsu_filter, black_filt
     tile_pen_fracs = filter_pen_reduced_image(slide, **pen_filter) if pen_filter is not None else np.zeros(tile_background_fracs.shape)
     filters_array_list = [tile_background_fracs, tile_black_fracs, tile_pen_fracs]
     num_filtered_per_filter = list(map(lambda f: f.sum(), filters_array_list))
-    print(len(tile_background_fracs), num_filtered_per_filter)
+    print(tile_background_fracs.shape, len(tile_background_fracs), num_filtered_per_filter)
     # empirical observation: it is commonly found that pen tiles tend to appear in large
     # amounts (when pen circles the tissue).
     # Few individual pen tiles are unlikely to be present. In case a few pen tiles is found it may be
@@ -119,7 +119,6 @@ def filter_non_tissue_tiles(slide, non_tissue_threshold, otsu_filter, black_filt
     tile_non_tissue_fracs_sum = sum(filters_array_list)
     tile_non_tissue_fracs_max = np.maximum.reduce(filters_array_list)
     filtered_tile_inds = np.argwhere(tile_non_tissue_fracs_sum > non_tissue_threshold)
-    print(len(filtered_tile_inds), filtered_tile_inds.sum())
     # for coloring - splitting filtered tiles into the most significant filter
     filtered_tile_array_inds = tuple(np.array(filtered_tile_inds).T)
     bg_tile_inds = filtered_tile_inds[tile_non_tissue_fracs_max[filtered_tile_array_inds] ==
