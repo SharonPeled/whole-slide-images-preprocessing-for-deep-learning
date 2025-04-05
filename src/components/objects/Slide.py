53aa0c148af2c4faca093d1a876f3520ad26a76d
sharon <ssharon95@gmail.com>
2025-04-05 16:05:33 +0300

    test sequantial access

--- src/components/objects/Slide.py
+++ src/components/objects/Slide.py
@@ -82,12 +82,8 @@ class Slide(Image):
                 downsample_f = 2 ** load_level[0]
                 Logger.log(f'Using manual downsampling: {downsample_f}', log_importance=2)
 
-                self.img_r = pyvips.Image.new_from_file(self.path).extract_band(0, n=3).resize(1.0 / downsample_f)
+                self.img_r = pyvips.Image.new_from_file(self.path, access='sequential').extract_band(0, n=3).resize(1.0 / downsample_f)
                 self.img_r_level = int(load_level[0])
-
-
-                print('Finished downsampling...')
-
         if self.img_r is None:
             Logger.log(f'Level not found: {load_level} and manual resizing failed.', log_importance=2)
             raise Exception(f"Loading level {load_level} failed.")
